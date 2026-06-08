654c2d36d71d97028cac39771dd098d489c79567
yhw <akdqlakdql9@gmail.com>
2026-06-08 17:12:44 +0900

    JAVA1

---
+++ JAVA/main1.js
@@ -0,0 +1,21 @@
+let n1 = 10 % 3;
+console.log(n1);
+
+let n2 = 2 ** 3;
+console.log(n2);
+
+let n3 = 1; 
+console.log(++n3);
+console.log(n3++);
+n3 = n3+n3;
+console.log(n3);
+
+let n4 = 10;
+let str = '10';
+let result = n4 == str;
+console.log(result);
+
+console.log(!result);
+
+let result1 = 10 < 20 ? '참입니다' : '거짓입니다';
+console.log(result1);
